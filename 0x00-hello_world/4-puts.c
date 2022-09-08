b2ff29baccb4c8603173315fe6126c8e062938be
Promise0000 <promiseolowo1@gmail.com>
2022-09-08 16:39:46 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
- #include <stdio.h>
+#include <stdio.h>
 
- /**
-  * main - entry
-  *  
-  * Return: Always 0 (Valid)
-  */
- int main(void)
- {  
+/**
+* main - entry
+*  
+* Return: Always 0 (Valid)
+*/
+int main(void)
+{  
 	  puts("\"Programming is like building a multilingual puzzle");
-	  return (0);
- } 
+          return (0);
+} 
