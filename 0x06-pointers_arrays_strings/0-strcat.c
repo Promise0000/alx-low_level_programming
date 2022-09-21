8d24bab6a1351644be661107b108539485fc1c2d
Promise0000 <promiseolowo1@gmail.com>
2022-09-21 13:14:07 -0700

    strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * strcat - Concatenates the string @src to the string @dest
+ * @dest: a pointer to the string
+ * @src: the source striong to be appended to @dest
+ *
+ * Return: a pointer ro the destination string @dest
+ */
+char *strcat(char *dest, const char *src)
+{
+	int index = 0, dest_len = 0;
+
+	while (desr[index++])
+		dest_len++;
+
+	for (index = 0; src[index]; index++)
+		dest[dest_len++] = src[index];
+
+	return (dest);
+}
